65454ecfc333701824158243f477d99cff655e17
JaviKo500 <javikogutierrez64@gmail.com>
2022-05-03 16:22:56 -0500

    responsive desing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://localhost:3000/api',
-  BASE_URL_SOCKET: 'http://localhost:3000'
+  BASE_URL: 'https://socket-chat-server-jk.herokuapp.com/api',
+  BASE_URL_SOCKET: 'https://socket-chat-server-jk.herokuapp.com'
 };
